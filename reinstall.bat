fbe6efe372b27c449bf67a6728837347fcd776b6
antermaz <antermaz@outlook.com>
2025-01-08 22:06:12 +0800

    fixed bugs

--- reinstall.bat
+++ reinstall.bat
@@ -186,7 +186,7 @@ echo Download: %1 %2
 del /q "%2" 2>nul
 if exist "%2" (echo Cannot delete %2 & exit /b 1)
 mkdir "%~dp2" 2>nul
-aria2c "%1" -o "%2" -c
+./aria2c.exe "%1" -o "%2" -c
 if %errorlevel% neq 0 exit /b 1
 exit /b
 
